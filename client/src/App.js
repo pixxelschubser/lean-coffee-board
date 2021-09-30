6c5a4ca541e141d5660f05281ea3c8e73247de30
Lars Radtke <lars.radtke@gmx.net>
2021-09-30 20:06:58 +0200

    swap data initialziation location from App to Main

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,9 @@
-import { useState } from 'react'
 import Main from './components/Main'
 
-const INITIAL_DATA = [
-  {
-    text: 'What is node.js?',
-    author: 'Max Mustermann',
-  },
-  {
-    text: 'How does routing work?',
-    author: 'Tina',
-  },
-  {
-    text: 'How to use mongo?',
-    author: 'Ron Weasley',
-  },
-  {
-    text: 'What is a server?',
-    author: 'Max Muster',
-  },
-]
-
 export default function App() {
-  const [data, setData] = useState(INITIAL_DATA)
-
   return (
     <>
-      <Main data={data} />
+      <Main />
     </>
   )
 }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,16 +1,57 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import Card from './Card';
 import Form from './Form';
 
-export default function Main({data}){
+const INITIAL_DATA = [
+  {
+	key: "1",
+    text: 'What is node.js?',
+    author: 'Max Mustermann',
+  },
+  {
+	key: "2",
+    text: 'How does routing work?',
+    author: 'Tina',
+  },
+  {
+	key: "3",
+    text: 'How to use mongo?',
+    author: 'Ron Weasley',
+  },
+  {
+	key: "4",
+    text: 'What is a server?',
+    author: 'Max Muster',
+  },
+]
+
+export default function Main(){
+	const [data, setData] = useState(INITIAL_DATA)
+
 	return (
 	<StyledMain>
 		{data.map((question) => (
-			<Card text={question.text} author={question.author} />
+			<Card key={question.key} text={question.text} author={question.author} />
 		))}
-		<Form />
+		<Form onCreateQuestion={createQuestionHandler}/>
 	</StyledMain>
 	)
+
+	function createQuestionHandler({text, author}) {
+		let i = data.length
+		i++
+		const newData = [
+			...data,
+			{
+				key: i,
+				text: text,
+				author: author
+			}
+		]
+		setData(newData)
+		console.log(i)
+	}
 }
 
 const StyledMain = styled.main`
