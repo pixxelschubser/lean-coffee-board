46621ef597c3822f227e642b632dfedd2fa270ff
Lars Radtke <lars.radtke@gmx.net>
2021-09-30 20:07:18 +0200

    remove unused imports

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import GlobalStyles from './GlobalStyles'
-import { useState, useEffect } from 'react'
 
 ReactDOM.render(
   <React.StrictMode>
