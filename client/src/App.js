7273090d40b15378c6d98f84099b286c7f42fbc9
Lars Radtke <lars.radtke@gmx.net>
2021-09-30 16:16:27 +0200

    create App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from './components/Main';
 
-function App() {
+const INITIAL_DATA = [
+  {
+    text: "What is node.js?",
+    author: "Max Mustermann"
+  },
+  {
+    text: "How does routing work?",
+    author: "Tina"
+  },
+  {
+    text: "How to use mongo?",
+    author: "Ron Weasley"
+  },
+  {
+    text: "What is a server?",
+    author: "Max Muster"
+  }
+]
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Main data={INITIAL_DATA}/>
+    </>
   );
 }
-
-export default App;
