1f1a09b44fb7d24c6598b571b49cc1056a0ed6ad
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-30 16:56:16 +0200

    Implement state variable

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,30 +1,30 @@
 import styled from 'styled-components'
 
-export default function Card({text, author}){
-	return (
-		<StyledArticle>
-			<StyledText>{text}</StyledText>
-			<StyledAuthor>{author}</StyledAuthor>
-		</StyledArticle>
-	)
+export default function Card({ text, author }) {
+  return (
+    <StyledArticle>
+      <StyledText>{text}</StyledText>
+      <StyledAuthor>{author}</StyledAuthor>
+    </StyledArticle>
+  )
 }
 
 const StyledArticle = styled.article`
-	width: 400px;
-	max-width: 80%;
-	margin-bottom: 20px;
-	padding: 20px;
-	border-radius: 20px;
-	border: 1px solid black;
-	display: inline-block;
+  width: 400px;
+  max-width: 80%;
+  margin-bottom: 20px;
+  padding: 20px;
+  border-radius: 20px;
+  border: 1px solid black;
+  display: inline-block;
 `
 
 const StyledText = styled.p`
-	font-size: 1.2rem;
-	margin-bottom: 20px;
+  font-size: 1.2rem;
+  margin-bottom: 20px;
 `
 
 const StyledAuthor = styled.p`
-	font-size: 1rem;
-	text-align: right;
+  font-size: 1rem;
+  text-align: right;
 `
