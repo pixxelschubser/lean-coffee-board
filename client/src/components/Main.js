d4dc8aabe8b1654cc235c738ab8c762002eee1f6
Sarah Schüßler <stschuessler@googlemail.com>
2021-10-01 15:25:33 +0200

    Switch commit

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,71 +1,73 @@
 import { useState } from 'react'
 import styled from 'styled-components/macro'
-import Card from './Card';
-import Form from './Form';
+import Card from './Card'
+import Form from './Form'
+import { nanoid } from 'nanoid'
 
 const INITIAL_DATA = [
   {
-	key: "1",
+    key: '1',
     text: 'What is node.js?',
     author: 'Max Mustermann',
   },
   {
-	key: "2",
+    key: '2',
     text: 'How does routing work?',
     author: 'Tina',
   },
   {
-	key: "3",
+    key: '3',
     text: 'How to use mongo?',
     author: 'Ron Weasley',
   },
   {
-	key: "4",
+    key: '4',
     text: 'What is a server?',
     author: 'Max Muster',
   },
 ]
 
-if (localStorage.getItem('localQuestions')===null){
-	localStorage.setItem('localQuestions', JSON.stringify(INITIAL_DATA))
+if (localStorage.getItem('localQuestions') === null) {
+  localStorage.setItem('localQuestions', JSON.stringify(INITIAL_DATA))
 }
- 
+
 let localData = JSON.parse(localStorage.getItem('localQuestions'))
 
+export default function Main() {
+  const [data, setData] = useState(localData)
 
+  return (
+    <StyledMain>
+      {data.map(question => (
+        <Card
+          key={question.key}
+          text={question.text}
+          author={question.author}
+        />
+      ))}
+      <Form onCreateQuestion={createQuestionHandler} />
+    </StyledMain>
+  )
 
+  function createQuestionHandler({ text, author }) {
+    let key = nanoid()
 
-export default function Main(){
-	const [data, setData] = useState(localData)
-
-	return (
-	<StyledMain>
-		{data.map((question) => (
-			<Card key={question.key} text={question.text} author={question.author} />
-		))}
-		<Form onCreateQuestion={createQuestionHandler}/>
-	</StyledMain>
-	)
-
-	function createQuestionHandler({text, author}) {
-		let i = data.length
-		i++
-		const newData = [
-			...data,
-			{
-				key: i,
-				text: text,
-				author: author
-			}
-		]
-		setData(newData)
-		console.log(i)
-	}
+    const newData = [
+      ...data,
+      {
+        key: key,
+        text: text,
+        author: author,
+      },
+    ]
+    localStorage.setItem('localQuestions', JSON.stringify(data))
+    setData(newData)
+  }
 }
 
 const StyledMain = styled.main`
-    padding: 20px;
-	width: 100%;
-	height: 100%;
-	white-space: wrap;
+  padding: 20px;
+  width: 100%;
+  height: 100%;
+  white-space: wrap;
 `
