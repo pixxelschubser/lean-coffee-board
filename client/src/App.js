ecf65bd558e4c85ae48d368f123dfa15a48a639b
Lars Radtke <lars.radtke@gmx.net>
2021-10-04 12:24:52 +0200

    connecting and fetching data from database

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react'
 import Main from './components/Main'
 
 export default function App() {
-  return (
-    <Main />
+
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    // GET http://localhost:4000/api/cards
+    fetch('/api/cards')
+      .then(response => response.json())
+      .then(data => setCards(data))
+      .catch(error => console.log(error))
+  }, [])
+  
+  return ( 
+    <Main cards={cards}/>
   )
 }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,46 +1,13 @@
-import { useState } from 'react'
 import styled from 'styled-components/macro'
 import Card from './Card'
 import Form from './Form'
 import { nanoid } from 'nanoid'
 
-const INITIAL_DATA = [
-  {
-    key: '1',
-    text: 'What is node.js?',
-    author: 'Max Mustermann',
-  },
-  {
-    key: '2',
-    text: 'How does routing work?',
-    author: 'Tina',
-  },
-  {
-    key: '3',
-    text: 'How to use mongo?',
-    author: 'Ron Weasley',
-  },
-  {
-    key: '4',
-    text: 'What is a server?',
-    author: 'Max Muster',
-  },
-]
-
-if (localStorage.getItem('localQuestions') === null) {
-  localStorage.setItem('localQuestions', JSON.stringify(INITIAL_DATA))
-}
-
-let localData = JSON.parse(localStorage.getItem('localQuestions'))
-
-export default function Main() {
-  const [data, setData] = useState(localData)
-
-  return (
+export default function Main({cards}) {return (
     <StyledMain>
-      {data.map(question => (
+      {cards.map(question => (
         <Card
-          id={question.key}
+          key={question.key}
           text={question.text}
           author={question.author}
         />
@@ -52,16 +19,16 @@ export default function Main() {
   function createQuestionHandler({ text, author }) {
     let key = nanoid()
 
-    const newData = [
-      ...data,
-      {
-        key: key,
-        text: text,
-        author: author,
-      },
-    ]
-	setData(newData)
-    localStorage.setItem('localQuestions', JSON.stringify(newData))
+  //   const newData = [
+  //     ...data,
+  //     {
+  //       key: key,
+  //       text: text,
+  //       author: author,
+  //     },
+  //   ]
+	// setData(newData)
+  //   localStorage.setItem('localQuestions', JSON.stringify(newData))
   }
 }
 
